36466f6faa999debf41285dfcc6ee95a87e41d18
Ryan Hefner <hi@ryanhefner.com>
2019-04-10 08:01:13 -0400

    Add babel config and setup next to reference items in src directory

---
+++ src/.next/static/development/pages/index.js
@@ -0,0 +1,114 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "../node_modules/@babel/runtime-corejs2/helpers/interopRequireDefault.js":
+/*!*******************************************************************************!*\
+  !*** ../node_modules/@babel/runtime-corejs2/helpers/interopRequireDefault.js ***!
+  \*******************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : {
+    default: obj
+  };
+}
+
+module.exports = _interopRequireDefault;
+
+/***/ }),
+
+/***/ "../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fryanhefner%2FDropbox%2FCode%2Fpersonal%2Fpkgstats%2Fsrc%2Fpages%2Findex.js!./":
+/*!***************************************************************************************************************************************************************************************************!*\
+  !*** ../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fryanhefner%2FDropbox%2FCode%2Fpersonal%2Fpkgstats%2Fsrc%2Fpages%2Findex.js ***!
+  \***************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var page = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.js */ "./pages/index.js", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+          next.router.update("/", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "../node_modules/react/index.js":
+/*!********************************************************************************************!*\
+  !*** delegated ../node_modules/react/index.js from dll-reference dll_5369eb564c9e983c3093 ***!
+  \********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_5369eb564c9e983c3093 */ "dll-reference dll_5369eb564c9e983c3093"))("../node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _interopRequireDefault = __webpack_require__(/*! @babel/runtime-corejs2/helpers/interopRequireDefault */ "../node_modules/@babel/runtime-corejs2/helpers/interopRequireDefault.js");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(__webpack_require__(/*! react */ "../node_modules/react/index.js"));
+
+var _jsxFileName = "/Users/ryanhefner/Dropbox/Code/personal/pkgstats/src/pages/index.js";
+
+function Home() {
+  return _react.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, "Welcome to Next.js!");
+}
+
+var _default = Home;
+exports.default = _default;
+
+/***/ }),
+
+/***/ 0:
+/*!******************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fryanhefner%2FDropbox%2FCode%2Fpersonal%2Fpkgstats%2Fsrc%2Fpages%2Findex.js ***!
+  \******************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2FUsers%2Fryanhefner%2FDropbox%2FCode%2Fpersonal%2Fpkgstats%2Fsrc%2Fpages%2Findex.js! */"../node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2FUsers%2Fryanhefner%2FDropbox%2FCode%2Fpersonal%2Fpkgstats%2Fsrc%2Fpages%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_5369eb564c9e983c3093":
+/*!*******************************************!*\
+  !*** external "dll_5369eb564c9e983c3093" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_5369eb564c9e983c3093;
+
+/***/ })
+
+},[[0,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=index.js.map
