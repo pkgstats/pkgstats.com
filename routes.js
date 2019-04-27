83f00126e3448efe91743046055af3b450dc79a8
Ryan Hefner <hi@ryanhefner.com>
2019-04-27 06:32:00 -0400

    Fix routing for namespaced packages

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const routes = require('next-routes');
 
 module.exports = routes()
   .add('/@:username', 'user')
-  .add('/pkg\::pkg', 'pkg')
+  .add('/pkg\::pkg(.*)', 'pkg')
   .add('/org\::organization', 'org')
   .add('/compare/:packages', 'compare')
   .add('/', 'index');
