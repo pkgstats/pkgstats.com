fad42f57e3d3d57f5785c3ffbad0899931cdc3ea
Ryan Hefner <hi@ryanhefner.com>
2019-06-05 22:02:58 -0400

    Clean-up code formatting

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,8 +4,8 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () => originalRenderPage({
