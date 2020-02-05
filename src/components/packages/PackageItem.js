c3077843c4755f38888fcac89f220efc1971a9b4
Ryan Hefner <hi@ryanhefner.com>
2020-02-05 12:07:59 -0500

    Update PackageItem to render package links as images to see if that helps with element counts

--- src/components/packages/PackageItem.js
+++ src/components/packages/PackageItem.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
@@ -122,20 +121,43 @@ const Package = styled.div`
     display: flex;
     justify-content: flex-end;
     align-items: center;
-
+  
     svg {
       transition: fill 0.2s ease-in-out, stroke 0.2s ease-in-out;
     }
 
+    img {
+      display: block;
+      margin: 0;
+      padding: 0;
+    }
+
     .package-link {
       padding: 0.7rem;
+
+      img {
+        height: 1.5rem;
+        opacity: 0.4;
+        transition: opacity 0.2s ease-in-out, stroke 0.2s ease-in-out;
+      }
 
       & + .package-link {
         margin-left: 0.5rem;
       }
     }
 
+    .package-link:active,
+    .package-link:hover {
+      img {
+        opacity: 1;
+      }
+    }
+
     .package-link--npm {
+      img {
+        height: 1.2rem;
+        margin-top: 0.2rem;
+      }
       svg {
         fill: #999;
         height: 1rem;
@@ -235,19 +257,27 @@ class PackageItem extends Component {
 
     ['npm', 'repository', 'homepage'].forEach(linkType => {
       if (pkg.package.links && pkg.package.links.hasOwnProperty(linkType)) {
-        let linkString = linkType;
+        // let linkString = linkType;
+        let linkTitle;
+        let linkIconUrl;
 
         switch (linkType) {
           case 'npm':
-            linkString = <svg viewBox="0 0 780 250"><path d="M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z"></path></svg>;
+            linkTitle = 'View on npm';
+            linkIconUrl = '/static/svgs/npm-icon.svg';
+            // linkString = <svg viewBox="0 0 780 250"><path d="M240,250h100v-50h100V0H240V250z M340,50h50v100h-50V50z M480,0v200h100V50h50v150h50V50h50v150h50V0H480z M0,200h100V50h50v150h50V0H0V200z"></path></svg>;
             break;
 
           case 'repository':
-            linkString = <svg viewBox="0 0 92 92" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>Git</title><g stroke="none"><path d="M90.155,41.965 L50.036,1.847 C47.726,-0.464 43.979,-0.464 41.667,1.847 L33.336,10.179 L43.904,20.747 C46.36,19.917 49.176,20.474 51.133,22.431 C53.102,24.401 53.654,27.241 52.803,29.706 L62.989,39.891 C65.454,39.041 68.295,39.59 70.264,41.562 C73.014,44.311 73.014,48.768 70.264,51.519 C67.512,54.271 63.056,54.271 60.303,51.519 C58.235,49.449 57.723,46.409 58.772,43.861 L49.272,34.362 L49.272,59.358 C49.942,59.69 50.575,60.133 51.133,60.69 C53.883,63.44 53.883,67.896 51.133,70.65 C48.383,73.399 43.924,73.399 41.176,70.65 C38.426,67.896 38.426,63.44 41.176,60.69 C41.856,60.011 42.643,59.497 43.483,59.153 L43.483,33.925 C42.643,33.582 41.858,33.072 41.176,32.389 C39.093,30.307 38.592,27.249 39.661,24.691 L29.243,14.271 L1.733,41.779 C-0.578,44.092 -0.578,47.839 1.733,50.15 L41.854,90.268 C44.164,92.578 47.91,92.578 50.223,90.268 L90.155,50.336 C92.466,48.025 92.466,44.275 90.155,41.965"></path></g></svg>;
+            linkTitle = 'View repository';
+            linkIconUrl = '/static/svgs/repository-icon.svg';
+            // linkString = <svg viewBox="0 0 92 92" version="1.1" xmlns="http://www.w3.org/2000/svg"><title>Git</title><g stroke="none"><path d="M90.155,41.965 L50.036,1.847 C47.726,-0.464 43.979,-0.464 41.667,1.847 L33.336,10.179 L43.904,20.747 C46.36,19.917 49.176,20.474 51.133,22.431 C53.102,24.401 53.654,27.241 52.803,29.706 L62.989,39.891 C65.454,39.041 68.295,39.59 70.264,41.562 C73.014,44.311 73.014,48.768 70.264,51.519 C67.512,54.271 63.056,54.271 60.303,51.519 C58.235,49.449 57.723,46.409 58.772,43.861 L49.272,34.362 L49.272,59.358 C49.942,59.69 50.575,60.133 51.133,60.69 C53.883,63.44 53.883,67.896 51.133,70.65 C48.383,73.399 43.924,73.399 41.176,70.65 C38.426,67.896 38.426,63.44 41.176,60.69 C41.856,60.011 42.643,59.497 43.483,59.153 L43.483,33.925 C42.643,33.582 41.858,33.072 41.176,32.389 C39.093,30.307 38.592,27.249 39.661,24.691 L29.243,14.271 L1.733,41.779 C-0.578,44.092 -0.578,47.839 1.733,50.15 L41.854,90.268 C44.164,92.578 47.91,92.578 50.223,90.268 L90.155,50.336 C92.466,48.025 92.466,44.275 90.155,41.965"></path></g></svg>;
             break;
 
           default:
-            linkString = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-globe"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>;
+            linkTitle = 'View package site';
+            linkIconUrl = '/static/svgs/globe-icon.svg';
+            // linkString = <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-globe"><circle cx="12" cy="12" r="10"></circle><line x1="2" y1="12" x2="22" y2="12"></line><path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path></svg>;
             break;
         }
 
@@ -258,8 +288,8 @@ class PackageItem extends Component {
             key={`package-link-${pkg.package.name}-${linkType}`}
             target="_blank"
             rel="noreferrer noopener"
-            title={linkType}
-          >{linkString}</a>
+            title={linkTitle}
+          ><img src={linkIconUrl} alt={linkType} /></a>
         ));
       }
     });
